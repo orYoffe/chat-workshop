c0f01741019e0054b86400e3872f0ff74b067a1c
orYoffe <ragiop07@gmail.com>
2019-10-07 10:23:34 +0200

    added username

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var port = process.env.PORT || 3000;
 
+const users = [];
+const messages = [];
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", function(socket) {
+  const username = socket.handshake.query.username;
+  users.push(username);
+  console.log(
+    "-------------socket.handshake.query.username----------",
+    socket.handshake.query.username
+  );
   socket.on("chat message", function(msg) {
-    io.emit("chat message", msg);
+    const newMessage = username + ": " + msg;
+    messages.push(newMessage);
+    io.emit("chat message", newMessage);
   });
 });
 
